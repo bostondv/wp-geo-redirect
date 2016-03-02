a857cf1c5440cb70f7f182678ffd8e7dbae29887
Boston Dell-Vandenberg <boston@pomelodesign.com>
2016-03-02 08:11:14 -0500

    Skip redirect for ajax requests

--- geo-redirect.php
+++ geo-redirect.php
@@ -6,7 +6,7 @@ Description: This plugin allows you to redirect your visitors or switch language
 Author: bostondv
 Author URI: http://pomelodesign.com
 Donate link: http://pomelodesign.com/donate
-Version: 1.1.0
+Version: 1.1.1
 License: MIT
 License URI: http://opensource.org/licenses/MIT
 Text Domain: wp-geo-redirect
@@ -145,11 +145,14 @@ class WP_Geo_Redirect {
           }
         }
       break;
-  }
+    }
   }
 
   public function checkIfRedirectNeeded() {
     $skip_redirect = false;
+
+    if ( defined('DOING_AJAX') && DOING_AJAX )
+      $skip_redirect = true;
 
     if ( $this->checkAdministrator() )
       $skip_redirect = true;
